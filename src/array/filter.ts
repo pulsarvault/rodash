f924ef3f608870a5ad6d6a2a6c2e09c3316f8bac
Rohit Chauhan <pulsarsafe@gmail.com>
2026-07-12 23:55:29 +0530

    ESM modules

--- src/array/filter.ts
+++ src/array/filter.ts
@@ -1,12 +1,16 @@
 // Rohit Chauhan: All Erasable types used. Just run .ts on Bun, Node or Deno
 // Filter Implementation
-function filter<T>(
-  items: T[],
-  keepit: (item: T, index: number) => boolean): T[] {
+export function filter<T>(
+  items: readonly T[],
+  shouldKeep: (item: T, index: number) => boolean): T[] {
   const result: T[] = []
   for (let index = 0; index < items.length; index++) {
-    if (keepit(items[index], index))
-      result.push(items[index])
+
+    const item = items[index]!
+
+    if (shouldKeep(item, index)) {
+      result.push(item)
+    }
   }
   return result
 }
--- src/array/map.ts
+++ src/array/map.ts
@@ -1,12 +1,15 @@
 // Rohit Chauhan: All Erasable types used. Just run .ts on Bun, Node or Deno
 // Map Implementation
-function map<T, U>(
-  items: T[],
+export function map<T, U>(
+  items: readonly T[],
   transform: (item: T, index: number) => U): U[] {
   const result: U[] = []
 
   for (let index = 0; index < items.length; index++) {
-    result.push(transform(items[index], index))
+
+    const item = items[index]!
+
+    result.push(transform(item, index))
   }
   return result
 }
